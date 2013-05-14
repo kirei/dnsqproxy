a446b6cabb97dc76194b9a4c1adf6fc21487b10f
Jakob Schlyter <jakob@kirei.se>
2013-05-14 10:34:41 +0100

    more tests

--- qtest.pl
+++ qtest.pl
@@ -3,31 +3,33 @@
 use strict;
 use JSON;
 
+my $nameserver = "8.8.8.8";
+
 my @queries = (
     {
-        address     => "8.8.8.8",
-        port        => 53,
-        tcp_timeout => 10,
-        udp_timeout => 10,
-        transport   => "udp",
-        bufsize     => 1024,
-        qname       => "kirei.se",
-        qclass      => "IN",
-        qtype       => "SOA",
-        flags       => { do => 0, cd => 0, rd => 1, ad => 0 },
+        address   => $nameserver,
+        transport => "tcp",
+        bufsize   => 1280,
+        qname     => "kirei.se",
+        qtype     => "SOA",
+        flags     => { do => 1, cd => 0, rd => 1, ad => 0 },
     },
     {
-        address     => "8.8.8.8",
-        port        => 53,
-        tcp_timeout => 10,
-        udp_timeout => 10,
-        transport   => "udp",
-        bufsize     => 1024,
-        qname       => "kirei.se",
-        qclass      => "IN",
-        qtype       => "MX",
-        flags       => { do => 1, cd => 0, rd => 1, ad => 0 },
-    }
+        address   => $nameserver,
+        transport => "udp",
+        bufsize   => 512,
+        qname     => "kirei.se",
+        qtype     => "NS",
+        flags     => { do => 0, cd => 0, rd => 1, ad => 0 },
+    },
+    {
+        address   => $nameserver,
+        transport => "udp",
+        bufsize   => 1024,
+        qname     => "kirei.se",
+        qtype     => "MX",
+        flags     => { do => 1, cd => 0, rd => 1, ad => 0 },
+    },
 );
 
 foreach my $json (map { to_json($_) } @queries) {
