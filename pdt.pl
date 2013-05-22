025a4cab95fda526cfa04b1645f4619b8f233cd9
Jakob Schlyter <jakob@kirei.se>
2013-05-22 12:51:08 +0200

    full .SE PDT example

--- pdt.pl
+++ pdt.pl
@@ -5,9 +5,27 @@ use warnings;
 use strict;
 use JSON;
 
-my $zone       = "";
-my @servers    = ("192.36.148.17", "2001:7fe::53");
-my $known_good = "se.$zone";
+my $zone    = "se";
+my @servers = (
+    "192.36.144.107",           # a.ns.se.
+    "2a01:3f0:0:301::53",       # a.ns.se.
+    "192.36.133.107",           # b.ns.se.
+    "2001:67c:254c:301::53",    # b.ns.se.
+    "192.36.135.107",           # c.ns.se.
+    "2001:67c:2554:301::53",    # c.ns.se.
+    "81.228.8.16",              # d.ns.se.
+    "81.228.10.57",             # e.ns.se.
+    "192.71.53.53",             # f.ns.se.
+    "2a01:3f0:0:305::53",       # f.ns.se.
+    "130.239.5.114",            # g.ns.se.
+    "2001:6b0:e:3::1",          # g.ns.se.
+    "194.146.106.22",           # i.ns.se.
+    "2001:67c:1010:5::53",      # i.ns.se.
+    "199.254.63.1",             # j.ns.se.
+    "2001:500:2c::1",           # j.ns.se.
+);
+
+my $known_good = "kirei.$zone";
 my $known_bad  = "4089e55b9193d26bfbbf968ea1283fe93f01f755.$zone";
 my $recursive  = "example.com";
 my @transports = ("udp", "tcp");
